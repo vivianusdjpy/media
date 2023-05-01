b29f142c12d6492d80cbe820bdb15be844a46d14
VIVIAN NJAU <viviannjau@users.noreply.github.com>
2023-05-01 20:54:14 +0200

    Skeleton loader

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
+
 function UsersList() {
-    return 'UsersList';
+    const dispatch = useDispatch();
+    const {isLoading, data, error} = useSelector((state) => {
+        return state.users;
+    });
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, [dispatch]);
+
+    if (isLoading) {
+        return <Skeleton times={6} className="h-10 w-full" />;
+    }
+    if (error) {
+        return <div>Error fetching data...</div>
+    }
+    return <div>{data.length}</div>;
 }
 
 export default UsersList;
