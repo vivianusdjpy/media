7a36cb2b396e84bab70a262e69d08caf20509e11
VIVIAN NJAU <viviannjau@users.noreply.github.com>
2023-05-05 00:46:59 +0200

    Albums list

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,51 +1,45 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from './Button';
 import Skeleton from "./Skeleton";
+import { useThunk } from '../hooks/use-thunk';
+import UsersListItem from "./UsersListItem";
 
 function UsersList() {
-    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-    const [loadingUsersError, setLoadingUsersError] = useState(null);
-    const dispatch = useDispatch();
+    const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+    const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
     const { data } = useSelector((state) => {
         return state.users;
     });
 
     useEffect(() => {
-        setIsLoadingUsers(true);
-        dispatch(fetchUsers())
-        .unwrap()
-        .catch((err) => setLoadingUsersError(err))
-        .finally(() => setIsLoadingUsers(false));
-    }, [dispatch]);
+        doFetchUsers();
+    }, [doFetchUsers]);
 
     const handleUserAdd = () => {
-        dispatch(addUser());
+        doCreateUser();
     };
 
+    let content;
     if (isLoadingUsers) {
-        return <Skeleton times={6} className="h-10 w-full" />;
+        content = <Skeleton times={6} className="h-10 w-full" />;
     }
     if (loadingUsersError) {
-        return <div>Error fetching data...</div>;
+        content = <div>Error fetching data...</div>;
+    } else {
+        content = data.map((user) => {     
+            return <UsersListItem key={user.id} user={user}/>       
+        });
     }
-    const renderedUsers = data.map((user) => {
-        return <div key={user.id} className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
-                {user.name}
-            </div>
-        </div>
-    });
 
     return <div>
-        <div className="flex flex-row justify-between m-3">
+        <div className="flex flex-row justify-between items-center m-3">
             <h1 className="m-2 text-xl">Users</h1>
-            <Button onClick={handleUserAdd}>
-                + Add User
-            </Button>
+            <Button loading={ isCreatingUser} onClick={handleUserAdd}> + Add User </Button>
+            {creatingUserError && 'Error creating user...'}
         </div>
-        {renderedUsers}</div>;
+        {content}</div>;
 }
 
 export default UsersList;
