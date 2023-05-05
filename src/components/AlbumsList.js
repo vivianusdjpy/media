c543d87ed65e1c3a94b4a6530d1fbbc0ce014c96
VIVIAN NJAU <viviannjau@users.noreply.github.com>
2023-05-05 18:44:51 +0200

    Redux query toolkit

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,31 @@
+import { useFetchAlbumsQuery } from "../store";
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
+
 function AlbumsList({ user }) {
-    return <div> Albums for {user.name} </div>;
+    const { data, error, isLoading} = useFetchAlbumsQuery(user);
+
+    let content;
+    if (isLoading) {
+        content = <Skeleton times={3}/>
+    } else if (error) {
+        content = <div>Error loading albums.</div>
+    } else {
+        content = data.map(album => {
+            const header = <div>{album.title}</div>;
+            
+            return <ExpandablePanel key={album.id} header={header}>
+                List of photos in the album
+            </ExpandablePanel>
+            
+        });
+    }
+
+    return <div>
+         <div>Albums for {user.name}</div> 
+         <div>{content}</div>
+         </div>;
 }
 
 export default AlbumsList;
